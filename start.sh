e309f7aceae72e0e9eac72c0cb9e31ae91207195
Nickolas Andersen <nicksen782@gmail.com>
2022-06-19 13:57:39 -0400

    The start.sh script now checks for lsof and kill before continuing.

--- start.sh
+++ start.sh
@@ -2,6 +2,14 @@
 
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+if !hash lsof 2>/dev/null; then
+  echo "lsof does not exist (install with: sudo apt install lsof)"
+fi
+
+if !hash kill 2>/dev/null; then
+  echo "kill does not exist (This should be standard in Linux.)"
+fi
+
 # echo "*****************************************"
 # echo "Checking if app port is already in use..."
 # echo "*****************************************"
